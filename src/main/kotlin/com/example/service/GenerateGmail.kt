c2f7479a90b3e90f551c55df9f897f9646dfeeea
Sharath083 <133197992+Sharath083@users.noreply.github.com>
2024-01-23 20:10:42 +0530

    Update GenerateGmail.kt

--- src/main/kotlin/com/example/service/GenerateGmail.kt
+++ src/main/kotlin/com/example/service/GenerateGmail.kt
@@ -13,7 +13,6 @@ class GenerateGmail {
 
             val email = MultiPartEmail()
 
-            // SMTP server settings for Gmail
             email.hostName = "smtp.gmail.com"
             email.setSmtpPort(465)
             email.setSSLOnConnect(true)
@@ -21,7 +20,7 @@ class GenerateGmail {
                 "sharath@s38"))
             email.isSSLOnConnect = true
 
-            // Email details
+            
             email.setFrom("sharath.r@marketsimplified.com")
             email.subject = "Sample Email with Attachment"
             email.setMsg("Hi bro")
@@ -36,47 +35,17 @@ class GenerateGmail {
             attachment.name = "Your Symbols.pdf"
             email.attach(attachment)
 
-            // Send the email
+            
             email.send()
 
-            println("Email sent successfully")
+        
         } catch (e: Exception) {
-            println("Failed to send email: ${e.message}")
-            e.printStackTrace()
+            throw CommonException(e.toString,HttpStatusCode.InternalServerError)
         }
 
 
     }
-    suspend fun generateEmptyGmail(uuid:String): MultiPartEmail {
-        val userMail=UserDaoImpl().getUserGmail(uuid)
-        println("=---------------$userMail")
-        val email = MultiPartEmail()
-        email.hostName = "smtp.gmail.com"
-        email.setSmtpPort(465)
-        email.setAuthenticator(DefaultAuthenticator("sharath.r@marketsimplified.com",
-            "sharath@s38"))
-        email.isSSLOnConnect = true
-        email.setFrom("sharath.r@marketsimplified.com")
-        email.subject = "Sample Email with Attachment"
-        email.setMsg("Hi bro")
-        email.addTo(userMail)
-
-
-        val templatePath = "D:\\BlankPdf.pdf"
-        GeneratePdf().generateEmptyPdf(uuid, templatePath)
-
-        val attachment = EmailAttachment()
-        attachment.path = "D:\\WatchList.pdf"
-        attachment.description = "Sample PDF Attachment"
-        attachment.name = "Your Symbols.pdf"
-
-        email.attach(attachment)
-        println("11111111113333333333333333333333333333333331111111111111")
-
-        email.send()
-        println("111111111122222222222222222222222222222111111111111111111111111111111111111111")
-
-        return email
-    }
+    
+    
 
 }
